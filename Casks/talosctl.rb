9ede869102c9746673dd84a4f2b26d2d819cfce4
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-03-29 21:11:46 +0300

    chore: update to 0.9.0
    Add a note to README.
    
    Signed-off-by: Alexey Palazhchenko <alexey.palazhchenko@gmail.com>

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -1,8 +1,8 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "0.8.5"
-  sha256 "41c992c73e22ddbf97e39b1fe1a85014c6ae87577314673d7e73462b8bfca13e"
+  version "0.9.0"
+  sha256 "c7298db732da518287f2209de79ff7f1e9e4d6f32f090450862400d2e2517b65"
 
   url "https://github.com/talos-systems/talos/releases/download/v#{version}/talosctl-darwin-amd64",
     verified: "github.com/talos-systems/talos/"
--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Installing talosctl from this cask is simple:
 - `brew tap talos-systems/talos`
 - `brew cask install talosctl`
 
+## Updates
+
+Currently, updates of this repository are manual
+(see [talos-systems/talos#2148](https://github.com/talos-systems/talos/issues/2148)) and may be delayed.
+If you rely on this tap, please send pull requests.
+
 ## Resources
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
