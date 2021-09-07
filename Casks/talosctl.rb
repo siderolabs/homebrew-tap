cbd42148fe954ca1f81208b86f9f241a60f50723
Serge Logvinov <serge.logvinov@sinextra.dev>
2021-09-07 20:21:45 +0300

    feat: add M1 support
    * add m1 (arm64) architecture support
    * update to v0.13.0
    
    Signed-off-by: Serge Logvinov <serge.logvinov@sinextra.dev>

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -1,15 +1,23 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "0.12.3"
-  sha256 "6b7de3d2decafee4acaa0f13d8eaf6fc96c7b4efbe4a9201f91f82106b98ae71"
+  version "0.13.0"
 
-  url "https://github.com/talos-systems/talos/releases/download/v#{version}/talosctl-darwin-amd64",
-    verified: "github.com/talos-systems/talos/"
+  if Hardware::CPU.intel?
+    sha256 "2719db0fa33c3337f9e2ad2aca289d208272b920b039cb731427ec16244c7159"
+    url "https://github.com/talos-systems/talos/releases/download/v#{version}/talosctl-darwin-amd64",
+      verified: "github.com/talos-systems/talos/"
+
+    binary "talosctl-darwin-amd64", target: "talosctl"
+  else
+    sha256 "48949176e6b399c3bdfeab6d12aac0b3b13374e837fe62c7e7c08bf7933b7529"
+    url "https://github.com/talos-systems/talos/releases/download/v#{version}/talosctl-darwin-arm64",
+      verified: "github.com/talos-systems/talos/"
+
+    binary "talosctl-darwin-arm64", target: "talosctl"
+  end
 
   name "talosctl"
   desc "A CLI for out-of-band management of Kubernetes nodes created by Talos"
   homepage "https://talos.dev"
-
-  binary "talosctl-darwin-amd64", target: "talosctl"
 end
