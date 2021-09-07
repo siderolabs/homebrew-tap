ca8545de294268a0b7b09b4cceea331930c0d949
Serge Logvinov <serge.logvinov@sinextra.dev>
2021-09-07 19:38:53 +0300

    chore: update to 0.12.1
    Just bump version and checksum.
    
    Signed-off-by: Serge Logvinov <serge.logvinov@sinextra.dev>
    Signed-off-by: Alexey Palazhchenko <alexey.palazhchenko@talos-systems.com>

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -1,8 +1,8 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "0.10.0"
-  sha256 "56aaea52fec5864cbfaf72befe3e10f9010478d1802bb870dfb8ea7dd0272a0c"
+  version "0.12.1"
+  sha256 "3e4f4a928f19f344d565a1a377ad681a16746ff4bef2dc52f6f4332f1b54f4a9"
 
   url "https://github.com/talos-systems/talos/releases/download/v#{version}/talosctl-darwin-amd64",
     verified: "github.com/talos-systems/talos/"
