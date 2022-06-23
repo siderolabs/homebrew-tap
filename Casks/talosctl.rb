bef4d82a40dca64dd8d4435b9c3e753b17879cc1
Serge Logvinov <serge.logvinov@sinextra.dev>
2022-06-23 22:00:39 +0300

    chore: update to v1.1.0
    Signed-off-by: Serge Logvinov <serge.logvinov@sinextra.dev>

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -1,16 +1,16 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "1.0.6"
+  version "1.1.0"
 
   if Hardware::CPU.intel?
-    sha256 "9b75b1faa3cf762039f50ae7c7ae58dc3f573136417c19c42fb84bf8977c6330"
+    sha256 "dd3aecd47df7db430d1a812b6839274fda06600249e47aa437c2f9a5bd74475f"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
       verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   else
-    sha256 "7d66db83ddd335a46f392e97173f9d034d9dfd8e68749651cddad924c7d0bb90"
+    sha256 "0a1348d9dd4167e7ff79e08c84e7f2a6de474a90626fdbfdc33bf9497ed83879"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
       verified: "github.com/siderolabs/talos/"
 
@@ -20,6 +20,6 @@ cask "talosctl" do
   name "talosctl"
   desc "A CLI for out-of-band management of Kubernetes nodes created by Talos"
   homepage "https://talos.dev"
-  
+
   zap trash: "~/.talos"
 end
