407ba1e9eb6e60128b733adcb150b02aebd9b40c
woolfyx <29287203+woolfyx@users.noreply.github.com>
2022-08-14 22:38:04 +0200

    Update to version 1.1.2
    Updating talosctl to v1.1.2 with brew cask checks

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -1,25 +1,28 @@
+# typed: false
+# frozen_string_literal: true
+
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "1.1.0"
+  version "1.1.2"
 
   if Hardware::CPU.intel?
-    sha256 "dd3aecd47df7db430d1a812b6839274fda06600249e47aa437c2f9a5bd74475f"
+    sha256 "d9d431a3a0f57c976792d4665f96448aa44f3eb81fe097504a885a89254446b9"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
-      verified: "github.com/siderolabs/talos/"
+        verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   else
-    sha256 "0a1348d9dd4167e7ff79e08c84e7f2a6de474a90626fdbfdc33bf9497ed83879"
+    sha256 "793d47f785bf96acb026265d8a7782f4424168a2783d5ec94babf1687046d86b"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
-      verified: "github.com/siderolabs/talos/"
+        verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-arm64", target: "talosctl"
   end
 
   name "talosctl"
-  desc "A CLI for out-of-band management of Kubernetes nodes created by Talos"
-  homepage "https://talos.dev"
+  desc "CLI for out-of-band management of Kubernetes nodes created by Talos"
+  homepage "https://talos.dev/"
 
   zap trash: "~/.talos"
 end
