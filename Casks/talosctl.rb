54305876066c65306cca1d23931be0e87cabd69b
Thorsten Schifferdecker <schifferdecker@b1-systems.de>
2022-09-01 14:44:20 +0200

    chore: update to v1.2.0
    Signed-off-by: Thorsten Schifferdecker <schifferdecker@b1-systems.de>

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -4,16 +4,16 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "1.1.2"
+  version "1.2.0"
 
   if Hardware::CPU.intel?
-    sha256 "d9d431a3a0f57c976792d4665f96448aa44f3eb81fe097504a885a89254446b9"
+    sha256 "5659949e0a09409ec421924f88f02194195c905a3ddbef2b1e68d2a00104b1e1"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
         verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   else
-    sha256 "793d47f785bf96acb026265d8a7782f4424168a2783d5ec94babf1687046d86b"
+    sha256 "36ad8abb0abcb09e5c726818a6cd1c40e9e1b89aa601924db21f2333f4966eb2"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
         verified: "github.com/siderolabs/talos/"
 
