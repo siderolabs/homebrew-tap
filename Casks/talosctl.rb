7ab7a157b13b87639ad60e80420830c3c51810a8
David Backeus <duztdruid@gmail.com>
2022-09-14 16:13:40 +0200

    Upgrade to 1.2.1

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -4,16 +4,16 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "1.2.0"
+  version "1.2.1"
 
   if Hardware::CPU.intel?
-    sha256 "5659949e0a09409ec421924f88f02194195c905a3ddbef2b1e68d2a00104b1e1"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
         verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   else
-    sha256 "36ad8abb0abcb09e5c726818a6cd1c40e9e1b89aa601924db21f2333f4966eb2"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
         verified: "github.com/siderolabs/talos/"
 
