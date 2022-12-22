1cf8fa00a5bfc2be1db08694ed144bf4b9c08199
el1191 <el1191@woyd.ru>
2022-12-22 11:19:50 +0300

    chore: update to v1.3.0

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -4,17 +4,17 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "1.2.6"
+  version "1.3.0"
 
   on_intel do
-    sha256 "a36db4992e050bca2362371a9623398db2e6a5f8d55357865b451572cf251716"
+    sha256 "c9c8a218da4fcb74c494a1f0d80182445824c3c2897cd77a9cf7cf4ecdb1bc18"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
         verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   end
   on_arm do
-    sha256 "fe04eef7be82a5326cdc4ce059433514f6d27efeb6b53cb686c0d7a630aa274d"
+    sha256 "06a6d2b46cc994ae63578b623b20a7de81a9451f6e74487c2dc851b5d3e57948"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
         verified: "github.com/siderolabs/talos/"
 
