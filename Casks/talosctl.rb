9f871d70fac556f8f00494952a6a2ee2de2cc2a1
Sander Maijers <3374183+sanmai-NL@users.noreply.github.com>
2023-01-25 10:49:34 +0100

    1.3.2 -> 1.3.3

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -4,17 +4,17 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "1.3.2"
+  version "1.3.3"
 
   on_intel do
-    sha256 "2ec5a4696197ede151cc72b3c34c1f1b24854f88bfb1c06f2c0aa3f3fccd1990"
+    sha256 "2f16b5a6c1821ee9e562b83fc514b9c0af2d8ed6b25e873d4ed7ad25ff18aa0e"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
         verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   end
   on_arm do
-    sha256 "8755bee1600ac7b906712d834acdee0bb30acf66b53c0cb0b38b0f5ccee57c81"
+    sha256 "f51d6039639f9792e84b36bdc2c53075e9ca1443cb6f504b84b9dc9a663e70f5"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
         verified: "github.com/siderolabs/talos/"
 
