5453a06d98f587cf47f79d437d5d37e84ee240fc
Steffen Windoffer <steffen@wind0r.de>
2023-03-09 13:09:53 +0100

    chore: update from 1.3.3 to 1.3.5

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -4,17 +4,17 @@
 # https://github.com/Homebrew/homebrew-cask/tree/master/doc/cask_language_reference
 
 cask "talosctl" do
-  version "1.3.3"
+  version "1.3.5"
 
   on_intel do
-    sha256 "2f16b5a6c1821ee9e562b83fc514b9c0af2d8ed6b25e873d4ed7ad25ff18aa0e"
+    sha256 "a0c0dff1b5bf3348894ebb1fb2bea6d68ddb8f126eab40b4538742d34ffe2355"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
         verified: "github.com/siderolabs/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   end
   on_arm do
-    sha256 "f51d6039639f9792e84b36bdc2c53075e9ca1443cb6f504b84b9dc9a663e70f5"
+    sha256 "de66fc7efd79dd999c911a33a0559f2d139e7eaf6cc2b36aa9bc447748bc2a70"
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
         verified: "github.com/siderolabs/talos/"
 
