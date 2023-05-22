595598f200f3af44660d6d4ca81b6144aad9e9dd
Joseph Page <josephpage@users.noreply.github.com>
2023-05-22 22:40:44 +0200

    feat!: add talosctl as Formula instead of Cask

---
+++ Formula/talosctl.rb
@@ -0,0 +1,51 @@
+# Copyright Contributors to the Talos project.
+# SPDX-License-Identifier: MPL-2.0
+
+class Talosctl < Formula
+  desc "CLI for out-of-band management of Kubernetes nodes created by Talos"
+  homepage "https://talos.dev/"
+  version "1.4.4"
+  license "MPL-2.0"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
+      verified: "github.com/siderolabs/talos/"
+    sha256 "3f167dad1e9539358856d0f73469d935a9eb89dc44c1cf5b6e03a1d6898d103f"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
+      verified: "github.com/siderolabs/talos/"
+    sha256 "4e22c3b60e7aa184d01acf489bb44ad4274e8ea92ae71e2abfc57395e112b9bf"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-amd64",
+      verified: "github.com/siderolabs/talos/"
+    sha256 "b4d7c96ed68fb91f566e6404ff3318aed019dc64efab3caa436557c4d59159c9"
+  elsif OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-armv7",
+      verified: "github.com/siderolabs/talos/"
+    sha256 "1f8ec7a58176cbb2ade8e89cf74d52b2e884fc4e289d819b709546989a356cff"
+  elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-arm64",
+      verified: "github.com/siderolabs/talos/"
+    sha256 "329637e668490cc0bbb54166e4058bc65aff840976854308588a111fc52e9b83"
+  else
+    odie "Unexpected platform!"
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "talosctl-darwin-amd64" => "talosctl"
+    elsif OS.mac? && Hardware::CPU.arm?
+      bin.install "talosctl-darwin-arm64" => "talosctl"
+    elsif OS.linux? && Hardware::CPU.intel?
+      bin.install "talosctl-linux-amd64" => "talosctl"
+    elsif OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      bin.install "talosctl-linux-armv7" => "talosctl"
+    elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      bin.install "talosctl-linux-arm64" => "talosctl"
+    end
+  end
+
+  test do
+    system "#{bin}/talosctl" "version"
+  end
+end
