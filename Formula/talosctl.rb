5e13ed0cf4716616e59ecab5efaaa1b1ea13c92d
Budlee <mat.auburn@gmail.com>
2023-06-06 20:05:22 +0100

    Add sha256 values to new version

--- Formula/talosctl.rb
+++ Formula/talosctl.rb
@@ -10,23 +10,23 @@ class Talosctl < Formula
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "3f167dad1e9539358856d0f73469d935a9eb89dc44c1cf5b6e03a1d6898d103f"
+    sha256 "09bd2c839e1d3b6f2c497612eeac703cf4a4baf812d8e4b1f821472a043bf435"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "4e22c3b60e7aa184d01acf489bb44ad4274e8ea92ae71e2abfc57395e112b9bf"
+    sha256 "a794227533ad94211e74a6b4746f661969c69448cec96fcd5cd6249276518b08"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-amd64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "b4d7c96ed68fb91f566e6404ff3318aed019dc64efab3caa436557c4d59159c9"
+    sha256 "cdf9e6f753da911763ca70f377db1b47f1ce8d0448921179035364d7a05fb8eb"
   elsif OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-armv7",
       verified: "github.com/siderolabs/talos/"
-    sha256 "1f8ec7a58176cbb2ade8e89cf74d52b2e884fc4e289d819b709546989a356cff"
+    sha256 "92804afb7b9a14acba605d80e9a2f2ff7430cb10524279f0ff0a7ffac3be1003"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-arm64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "329637e668490cc0bbb54166e4058bc65aff840976854308588a111fc52e9b83"
+    sha256 "bf93060dea48471043ffd401b871ee5dbf5145b7622296e60308d25eed3ecd14"
   else
     odie "Unexpected platform!"
   end
