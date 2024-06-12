81edcd86f5d58b748d855698a21c4c823833aced
UnknownBlunders <ethannor@gmail.com>
2024-06-12 17:08:45 +0000

    chore: update omnictl to v0.37.4

--- Formula/omnictl.rb
+++ Formula/omnictl.rb
@@ -4,25 +4,25 @@
 class Omnictl < Formula
   desc "CLI for Omni - SaaS-simple deployment of Kubernetes - on your own hardware."
   homepage "https://omni.siderolabs.com/"
-  version "0.37.2"
+  version "0.37.4"
   license "BUSL-1.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-darwin-amd64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "041a603468045cffd461f34c052632b6eac4eeaa2c379cf4fa9a249c4c494791"
+    sha256 "5dab26e2e6abf3b136fdd0ca5f53eaf1042eb9fcfb885c493d81064f1b5f52a9"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-darwin-arm64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "e47ae1325957e1476b55ba8b7f9c477134736dbd0f6900faedeee50073603b7c"
+    sha256 "ee8fea8bc1d60c0aad0012e69ca9ea05b32cc4322de5d44e7da0875d0e354db9"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-linux-amd64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "487a3b6738b1455fb74e8cf24294dc022a55981df8008feb522c3d606f6253f6"
+    sha256 "d42798d3b9400e49469e8e70f2f6dfbc4d935e684fa4944075ff5872743390c5"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-linux-arm64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "0b81aea4202de0b1c0b590a9c39a0dbae8db6dbac69f9789195bd908a0fa43ba"
+    sha256 "8723aaf290d26d9230a926749134cd418f6b4c84da97ba4ab527610f5dca5dfa"
   else
     odie "Unexpected platform!"
   end
