b5c25fde351783c40df3780b100ed7cec0d6f02c
UnknownBlunders <ethannor@gmail.com>
2024-12-17 15:49:08 +0000

    chore: update talosctl to v1.9.0

--- Formula/talosctl.rb
+++ Formula/talosctl.rb
@@ -4,29 +4,29 @@
 class Talosctl < Formula
   desc "CLI for out-of-band management of Kubernetes nodes created by Talos"
   homepage "https://talos.dev/"
-  version "1.8.4"
+  version "1.9.0"
   license "MPL-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "c1a53f14cb7445280075d200a9883b74a925d71c15fdf8ea81ac1c7614938245"
+    sha256 "1428f3008196db3245ba03eedc01a3c3e8848328a2c0d908504e5ecf960eed45"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "04ceb1324ae10a8205cf8396888d373b8b27cfbb292ff517fceabfbc3459c471"
+    sha256 "d20bc5107f4804423c2be8431bf5f7afe48cc42e076c75006e440218f56dca8e"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-amd64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "7189408732ce918db13b6082e3dda9f2747e7e4a7da23d3642c8f352fc4b2072"
+    sha256 "a06ebd06488067921f0f40732fcc52b8d977e3c86c80d0502f73783c338dc25b"
   elsif OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-armv7",
       verified: "github.com/siderolabs/talos/"
-    sha256 "a7bf85370162f4d17ab815d330705cad5e6595c977351c3579c3a30f544ba86f"
+    sha256 "b81459574e7035a6f0c6e238a8f8a58a81668103322b380d23e321ba35c10313"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-arm64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "aa3ff030a1abb706726bb6c35f96f031567836c84c9f992feda0deb9ec146d19"
+    sha256 "405ca47fcd659206ecb1e73f8bb817b9c9352d3929c03608d8a81f53c472f662"
   else
     odie "Unexpected platform!"
   end
