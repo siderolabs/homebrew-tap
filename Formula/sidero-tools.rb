3ecdf1b4e733a138a685e7b54d27056e185c9f8c
Justin Garrison <justinleegarrison@gmail.com>
2025-11-13 21:08:19 -0500

    feat: bump sidero tools version
    Needed for clients to fetch updates.

--- Formula/sidero-tools.rb
+++ Formula/sidero-tools.rb
@@ -1,6 +1,6 @@
 class SideroTools < Formula
     desc "Meta Package for common Sidero Labs tools"
-    version "1.0.0"
+    version "1.1.0"
     homepage "https://github.com/siderolabs/"
     url "file:///dev/null"
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
