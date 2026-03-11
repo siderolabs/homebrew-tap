70232cc1cf34daeb3ecb77b6bb278d21332a6d11
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2026-03-11 16:55:48 +0400

    feat: update talosctl to 1.12.5
    The latest version.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

--- Formula/talosctl.rb
+++ Formula/talosctl.rb
@@ -4,29 +4,29 @@
 class Talosctl < Formula
   desc "CLI for out-of-band management of Kubernetes nodes created by Talos"
   homepage "https://talos.dev/"
-  version "1.12.4"
+  version "1.12.5"
   license "MPL-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "c0b7283352ea0ce7d6435c663727a2cfef7f504c9b404f982b2f960501f3d9c8"
+    sha256 "8a58a951144d7f1a8eec4b6bb725553e4cf0d9c5b66785a8e0247890ec144f10"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "f4c94d4832e9d41f67cc63cd362cc0bfd44d8c675e9ff04541411be40916096c"
+    sha256 "8e4adec89acea650008f65b5e53c601a5115b3b5d6b6d15a8c215e533451b8a6"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-amd64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "6b85f633721e02d31c8a28a633c9cd8ebfb7e41677ff29e94236a082d4cd6cd9"
+    sha256 "3fbd456ec98f96dca42de0528220898177077bf023e3ae9432fb90a2dbe8cf66"
   elsif OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-armv7",
       verified: "github.com/siderolabs/talos/"
-    sha256 "1a78c6752ec3613062de93aa989ac7603eeb866ec7a76e2e43cc8bfe4ce7e554"
+    sha256 "e454e853794abddb8c4f98ef2fa7daffdedba292b64062dc2b66e12e8ab3daf8"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-linux-arm64",
       verified: "github.com/siderolabs/talos/"
-    sha256 "5d6f37684d337a9b689216a4e7cc07b58996bad34afbd9d032ab1bd058cfe282"
+    sha256 "108b1745ddb98838b15537e6f7951d25cfaafbda62c5740d38273ddb1aea0fb1"
   else
     odie "Unexpected platform!"
   end
