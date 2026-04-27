c715c1321869eb9fb31124bd7714443aa4e73f39
Artem Chernyshev <artem.chernyshev@talos-systems.com>
2026-04-27 14:48:07 +0300

    chore: bump omnictl to v1.7.1
    Bump omnictl to v1.7.1
    
    Signed-off-by: Artem Chernyshev <artem.chernyshev@talos-systems.com>

--- Formula/omnictl.rb
+++ Formula/omnictl.rb
@@ -4,25 +4,25 @@
 class Omnictl < Formula
   desc "CLI for Omni - SaaS-simple deployment of Kubernetes - on your own hardware."
   homepage "https://omni.siderolabs.com/"
-  version "1.7.0"
+  version "1.7.1"
   license "BUSL-1.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-darwin-amd64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "de6c68e3794a0e07747f4883dd212cb51fa25823a7be074038b27dfff1394643"
+    sha256 "a64e8655c7f9a4d3b3febdec4c35b00b539a20d2237bba37a2722b3c7d2661b8"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-darwin-arm64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "a4843788693374672d3864965993bcbde594add70257c07523f82df2e82857cf"
+    sha256 "3d3da77b994212525f4f1f1b74cd5d782c67d6a7ee5bd3ed81fe2181ca72419d"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-linux-amd64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "8baa468916e43e62d1e290aeb3d8c6b7ad053738cf2136ae33f3678f417546b4"
+    sha256 "342310ef935b6eb983ac26dae4782aa1429923081bb644b27cb24fea45a15e49"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-linux-arm64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "8ea2d465b3d58562839ad2111a77e7dbc80fb6986474a52933684cadfe2d62bb"
+    sha256 "f5c9d78c0a20a5b667eaaa025806a37cb14d0235d7470a1f2880f1fd32f38249"
   else
     odie "Unexpected platform!"
   end
