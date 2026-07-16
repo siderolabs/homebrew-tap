e95f7d70f721d7c3a39cb20d3241d36aa732d807
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2026-07-16 14:26:32 +0300

    chore: bump omnictl to v1.9.2
    See https://github.com/siderolabs/omni/releases/tag/v1.9.2.
    
    Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

--- Formula/omnictl.rb
+++ Formula/omnictl.rb
@@ -4,25 +4,25 @@
 class Omnictl < Formula
   desc "CLI for Omni - SaaS-simple deployment of Kubernetes - on your own hardware."
   homepage "https://omni.siderolabs.com/"
-  version "1.9.1"
+  version "1.9.2"
   license "BUSL-1.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-darwin-amd64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "a6e2e960614c7dc819c95ccd2ca39ca8953a9666f830bacabd18d1aaea74cb68"
+    sha256 "3e2e896b000ee73d6ada4e228d00ec59db4fee5b544ff6aad8504e56a835d986"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-darwin-arm64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "e9e85e5fb15036af07a8912ddf0271598c52605f71ce12bef264445a98821ec0"
+    sha256 "bc99fcc2b1aab16abc0f4c03eed741da6bf8198d4b2b8b7f5d757e3ef05b5ae3"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-linux-amd64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "66c7a844c45e01fa5933f8b9db56c19d23eca65db441d56d0312316e683bb8e2"
+    sha256 "5992ec1ee1182774ecedd25095b79d5bb12c5996abe63e0d24d16d7e72fcec18"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/siderolabs/omni/releases/download/v#{version}/omnictl-linux-arm64",
       verified: "github.com/siderolabs/omni/"
-    sha256 "9c10475ebcfe17a6276fa8e149076937a1a91492efc272b62eb5a03c37f0c7ec"
+    sha256 "3322a0a42cdf5458d1661ad056c00f872c9ee1e08aa5cc7ecd6ad2ed60d462bd"
   else
     odie "Unexpected platform!"
   end
